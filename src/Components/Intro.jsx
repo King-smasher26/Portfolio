a1cac14035cc130ae7e34eb9e6fa01e475c5bef2
King-smasher26 <yoab100203@gmail.com>
2024-08-06 02:07:25 +0530

    some more changes in intro

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -54,7 +54,7 @@ const Intro = () => {
       <ContentWrapper>
         <Avatar
           src={`${process.env.PUBLIC_URL}/avatar-portfolio.jpg`}
-          alt="Your Name"
+          alt="Aman Bisht"
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
           transition={{ duration: 0.5 }}
@@ -64,7 +64,7 @@ const Intro = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.3, duration: 0.5 }}
         >
-          Your Name
+          Aman Bisht
         </Name>
         <Tagline
           initial={{ opacity: 0, y: 20 }}
